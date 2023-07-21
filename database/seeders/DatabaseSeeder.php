32892032e0fa14c3cef9c0fe2b61e830a11e12fd
askar <mr.askar1984@gmail.com>
2023-07-21 15:23:11 +0300

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
 
              UserSeeder::class,
          CategoriesTableSeeder::class,
-          SubcategoriesTableSeeder::class,
+          SubCategoryTableSeeder::class,
 
         ]);
     }
